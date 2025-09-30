9b28899be8f52a54bcd2db597e0144808fbb575a
Mikko Harakk <mj.harakka@gmail.com>
2025-09-30 16:33:55 +0300

    refactored to use NavigationBar instead of NavigationRail

--- lib/main.dart
+++ lib/main.dart
@@ -1,124 +1,98 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
-}
+/// Flutter code sample for [NavigationBar].
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() => runApp(const NavigationBarApp());
+
+class NavigationBarApp extends StatelessWidget {
+  const NavigationBarApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Pet Grooming App',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 255, 86, 86),
+    return const MaterialApp(home: Navigation());
+  }
+}
+
+class Navigation extends StatefulWidget {
+  const Navigation({super.key});
+
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.amber,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(icon: Icon(Icons.home_outlined), label: 'Home'),
+          NavigationDestination(
+            icon: Icon(Icons.collections_bookmark),
+            label: 'Appointments',
           ),
-          fontFamily: 'RobotoMono',
+          NavigationDestination(icon: Icon(Icons.store), label: 'Store'),
+          NavigationDestination(
+            icon: Icon(Icons.person_outline),
+            label: 'Profile',
+          ),
+        ],
+      ),
+      body: <Widget>[
+        /// Home page
+        Card(
+          shadowColor: Colors.transparent,
+          margin: const EdgeInsets.all(8.0),
+          child: SizedBox.expand(
+            child: Center(
+              child: Text('Home page', style: theme.textTheme.titleLarge),
+            ),
+          ),
         ),
-        home: MyHomePage(),
-      ),
-    );
-  }
-}
 
-//No state currently
-class MyAppState extends ChangeNotifier {}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    Widget page;
-    switch (selectedIndex) {
-      case 0:
-        page = GeneratorPage();
-      case 1:
-        page = AboutPage();
-      default:
-        throw UnimplementedError('no widget for $selectedIndex');
-    }
-
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Scaffold(
-          body: Row(
-            children: [
-              SafeArea(
-                child: NavigationRail(
-                  extended: constraints.maxWidth >= 600, // ← Here.
-                  destinations: [
-                    NavigationRailDestination(
-                      icon: Icon(Icons.home),
-                      label: Text('Home'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.favorite),
-                      label: Text('About'),
-                    ),
-                  ],
-                  selectedIndex: selectedIndex,
-                  onDestinationSelected: (value) {
-                    setState(() {
-                      selectedIndex = value;
-                    });
-                  },
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  child: page,
-                ),
-              ),
-            ],
+        // Appointments
+        Card(
+          shadowColor: Colors.transparent,
+          margin: const EdgeInsets.all(8.0),
+          child: SizedBox.expand(
+            child: Center(
+              child: Text('Appointments', style: theme.textTheme.titleLarge),
+            ),
           ),
-        );
-      },
-    );
-  }
-}
+        ),
 
-class AboutPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Welcome to [Business Name] Where tails wag and pets shine! Our trusted grooming experts make sure your furry friend not only looks their best but feels their best. From quick washes to full spa treatments, booking your pet’s next pampering session is just a tap away.',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+        /// Store page
+        Card(
+          shadowColor: Colors.transparent,
+          margin: const EdgeInsets.all(8.0),
+          child: SizedBox.expand(
+            child: Center(
+              child: Text('Store', style: theme.textTheme.titleLarge),
+            ),
           ),
-        ],
-      ),
-    );
-  }
-}
+        ),
 
-class GeneratorPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Founded out of a love for animals, we created a space where grooming goes beyond just a haircut—it’s about comfort, care, and joy. Our team of trained professionals offers tiered grooming packages (Silver, Gold, Platinum) designed to match every pet’s needs, from a refreshing bath to a full luxury spa experience. With our new mobile-first platform, you can: Create your own pet profiles with breed, age & preferences Browse grooming packages and prices Book appointments in real-time with instant confirmation (Coming soon) Shop our curated collection of pet care products Our mission is simple: to make pets happy and owners worry-free.',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+        // Profile
+        Card(
+          shadowColor: Colors.transparent,
+          margin: const EdgeInsets.all(8.0),
+          child: SizedBox.expand(
+            child: Center(
+              child: Text('Profile', style: theme.textTheme.titleLarge),
+            ),
           ),
-        ],
-      ),
+        ),
+      ][currentPageIndex],
     );
   }
 }
