9907b4fe006cdbd2296130af5cbf230bdf3c7e33
Elia Pfammatter <elia.pfammatter@students.hevs.ch>
2025-10-01 08:54:41 +0200

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,16 @@ import 'package:flutter_app/pages/appointments.dart';
 import 'package:flutter_app/pages/home.dart';
 import 'package:flutter_app/pages/profile.dart';
 import 'package:flutter_app/pages/store.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(const App());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const App());
+}
 
 class App extends StatelessWidget {
   const App({super.key});
